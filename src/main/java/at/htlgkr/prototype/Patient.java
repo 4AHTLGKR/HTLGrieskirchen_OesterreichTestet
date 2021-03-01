fa71b08cc344ee51b128dd2016e5a4172e014e09
Jan Führer <jan_fue@icloud.com>
2021-03-01 13:45:38 +0100

    added model

---
+++ src/main/java/at/htlgkr/prototype/Patient.java
@@ -0,0 +1,31 @@
+package at.htlgkr.prototype;
+
+public class Patient {
+    private long id;
+    private String firstname;
+    private String lastname;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
