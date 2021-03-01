265ddcc22bc9071ee16a593dd14cde54611aab82
Jan Führer <jan_fue@icloud.com>
2021-03-01 13:49:27 +0100

    added submit method

--- src/main/java/at/htlgkr/prototype/PatientController.java
+++ src/main/java/at/htlgkr/prototype/PatientController.java
@@ -3,6 +3,8 @@ package at.htlgkr.prototype;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PatientController {
@@ -10,5 +12,11 @@ public class PatientController {
     public String patientForm(Model model) {
         model.addAttribute("patient", new Patient());
         return "addPatient";
+    }
+
+    @PostMapping("/")
+    public String patientSubmit(@ModelAttribute Patient patient, Model model) {
+        model.addAttribute("patient", patient);
+        return "addedPatient";
     }
 }
