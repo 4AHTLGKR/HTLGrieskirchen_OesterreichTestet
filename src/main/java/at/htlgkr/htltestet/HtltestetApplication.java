57fffbbc47c17efd6fe01af725885364ebdbc18c
Jan Führer <jan_fue@icloud.com>
2021-04-12 11:02:42 +0200

    init the project

---
+++ src/main/java/at/htlgkr/htltestet/HtltestetApplication.java
@@ -0,0 +1,13 @@
+package at.htlgkr.htltestet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtltestetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtltestetApplication.class, args);
+    }
+
+}
