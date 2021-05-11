7261a8597bb00bf433d883961caa67e8e64561bf
LukasSchmidt02 <schmidt.lukas2002@outlook.com>
2021-05-11 15:14:05 +0200

    applied barcode changes from pdf_creation_framework branch

--- src/main/java/at/htlgkr/htltestet/barcode/BarcodeController.java
+++ src/main/java/at/htlgkr/htltestet/barcode/BarcodeController.java
@@ -13,6 +13,11 @@ public class BarcodeController {
     @RequestMapping(produces = MediaType.IMAGE_JPEG_VALUE, method = RequestMethod.GET, value = "/barcode")
     public @ResponseBody
     byte[] getBarcode(@RequestParam String code, @RequestParam(defaultValue = "50") int height) throws Exception {
+        return getBarcodeStatic(code, height);
+    }
+
+    public static byte[] getBarcodeStatic(String code, int height) throws Exception
+    {
         BufferedImage img = BarcodeCreator.generateCode128BarcodeImage(code, height);
         ByteArrayOutputStream bao = new ByteArrayOutputStream();
         ImageIO.write(img, "jpg", bao);
