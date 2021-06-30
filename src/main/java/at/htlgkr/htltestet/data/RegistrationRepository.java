f8b29b4fe26a3429cce84ff9ff3ef743891db4b2
dlienbacher17 <lienbacherd@outlook.com>
2021-06-30 13:55:45 +0200

    Revert "Fixed RegistrationRespository"
    This reverts commit 46c000c74002abbf1fd11940acde2e041d9b3c76.

--- src/main/java/at/htlgkr/htltestet/data/RegistrationRepository.java
+++ src/main/java/at/htlgkr/htltestet/data/RegistrationRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RegistrationRepository extends JpaRepository<RegistrationData, Long> {
+public interface RegistrationRepository extends JpaRepository<Registration, Long> {
 
 
 
