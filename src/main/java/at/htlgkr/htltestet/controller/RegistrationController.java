103516f1d357db2723137cdbf29c0a7dad0b8776
LukasSchmidt02 <schmidt.lukas2002@outlook.com>
2021-06-30 12:52:57 +0200

    Created Site 10 (Database?)

--- src/main/java/at/htlgkr/htltestet/controller/RegistrationController.java
+++ src/main/java/at/htlgkr/htltestet/controller/RegistrationController.java
@@ -1,11 +1,16 @@
 package at.htlgkr.htltestet.controller;
 
 import at.htlgkr.htltestet.data.Registration;
+import at.htlgkr.htltestet.pdf.RegistrationPDF;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Controller
 public class RegistrationController {
@@ -34,6 +39,16 @@ public class RegistrationController {
         return "Booking/Completed";
     }
 
-
+    @GetMapping("appointment")
+    public String appointment(@ModelAttribute("registration") Registration registration, Model model, @ModelAttribute("registrationPDF") RegistrationPDF registrationPDF) {
+        model.addAttribute("registration", registration);
+        RegistrationPDF rpdf = new RegistrationPDF();
+        rpdf.setCode((int)(Math.random()*10000000) + "");
+        rpdf.setScreeningStation("Schulzentrum - Raiffeisen Arena, Grieskirchen \n 4710 Grieskirchen \n Parzer Schulstraße 1");
+        model.addAttribute("registrationPDF", rpdf);
+        registration.setTestDateTime(LocalDateTime.of(LocalDate.of(2021, 6, 30),LocalTime.of(12,47)));
+        model.addAttribute("testDate", registration.testDateTime.toLocalDate());
+        return "Booking/AppointmentVerification";
+    }
 
 }
