1f0c3dbb1948b6c35aa3ef3132d0715f1e6ce9e9
daniel <daniel.lehner030303@gmail.com>
2021-06-30 08:52:27 +0200

    removed unneccessary code and logs

--- src/main/resources/static/JS/script.js
+++ src/main/resources/static/JS/script.js
@@ -1,32 +1,17 @@
-console.log("fdfagwre");
-
-let birthdate;
-
-let screeningStationId;
-
-
-$('#birthdate').on('input', function(){
-    birthdate=$(this).val();
-    console.log($(this).val());
-});
 
 $('.screen-place li').click(function() {
     $(".selected-screeningstation").html($(this).text());
     $('#selectedScreeningStationId').val($(this).children('input').first().val());
-    console.log($('#selectedScreeningStationId').val());
-    console.log($(this).children('input').first().val());
     $(this).toggleClass('active').siblings().removeClass('active');
 });
 
 $('.time-list li').click(function() {
     $(this).toggleClass('active').siblings().removeClass('active');
     changeDateTime();
-    console.log($('#selectedTimeSlot').val());
 });
 
 $('#dateSlot').on('input', function(){
     changeDateTime();
-    console.log($('#selectedTimeSlot').val());
 });
 
 function changeDateTime()
