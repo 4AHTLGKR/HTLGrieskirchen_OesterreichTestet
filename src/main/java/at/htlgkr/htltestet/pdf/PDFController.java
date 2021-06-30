5a04eea89a970d295c913759fabca4936fd9dd86
Jan Führer <jan_fue@icloud.com>
2021-06-30 09:44:07 +0200

    changed controller action

--- src/main/java/at/htlgkr/htltestet/pdf/PDFController.java
+++ src/main/java/at/htlgkr/htltestet/pdf/PDFController.java
@@ -4,6 +4,7 @@ package at.htlgkr.htltestet.pdf;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import java.io.IOException;
@@ -12,8 +13,8 @@ import java.io.IOException;
 @CrossOrigin
 public class PDFController {
 
-    @RequestMapping(produces = MediaType.APPLICATION_PDF_VALUE, method = RequestMethod.GET, value = "/pdf/result")
-    public byte[] getResult(ResultPDF result){
+    @RequestMapping(produces = MediaType.APPLICATION_PDF_VALUE, method = RequestMethod.POST, value = "/pdf/result")
+    public byte[] getResult(@RequestBody ResultPDF result){
         try {
             return result.createPDF();
         } catch (IOException e) {
@@ -22,8 +23,8 @@ public class PDFController {
         return null;
     }
 
-    @RequestMapping(produces = MediaType.APPLICATION_PDF_VALUE, method = RequestMethod.GET, value = "/pdf/registration")
-    public byte[] getRegistration(RegistrationPDF registration){
+    @RequestMapping(produces = MediaType.APPLICATION_PDF_VALUE, method = RequestMethod.POST, value = "/pdf/registration")
+    public byte[] getRegistration(@RequestBody RegistrationPDF registration){
 
         try {
             return registration.createPDF();
