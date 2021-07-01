0c09ce590413454f07ece341e63155709dac5abd
DawnOfDiscovery <fabianpsutka@gmx.net>
2021-07-01 11:28:11 +0200

    Update data.sql
    Added space in file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO screeningstations (`id`, `city`,`plz`,`street`,`name`) VALUES ('0', 'Grieskirchen', '4710', 'Parzer Schulstraße 1', 'Schulzentrum - Raiffeisen Arena, Grieskirchen');
+
 INSERT INTO registrations (`id`,`firstname`, `lastname`, `birthdate`, `gender`, `street`, `street_number`,`plz`, `place`, `phone_number`, `email`, `screening_Station_Id`, `test_Date_Time`) VALUES ('0','Hans','Huber', '2000-02-03', '0', 'Dürerstrasse', '10', '4614', 'Marchtrenk', '06606624983', 'mann@fraum.com', '0', '2021-07-16');
